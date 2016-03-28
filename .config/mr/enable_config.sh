ce292b652b488fff66b1bde42348bb6ae04681ab
Judson <nyarly@gmail.com>
2016-03-28 21:08:26 +0100

    Fixed the enable config script

--- .config/mr/enable_config.sh
+++ .config/mr/enable_config.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 if [ $# -lt 1 ]; then
   echo "Usage: $0 <available config.ext>+"
@@ -7,8 +8,18 @@ fi
 mrpath=$(dirname $0)
 configpath="${mrpath}/config.d/"
 
-mkdir "$configpath"
+mkdir -p "$configpath"
 
-for f in "$*"; do
-  cp "${f}" "$configpath"
+cd "$configpath"
+
+for f in "$@"; do
+  name=$(basename "$f")
+  availpath="../available.d/${name}"
+
+  if [ -f "$availpath" ]; then
+    ln -sf "$availpath" "$name"
+  else
+    pwd
+    echo "Warning: no such file:" ${availpath}
+  fi
 done
