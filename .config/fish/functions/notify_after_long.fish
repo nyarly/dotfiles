decdf1bd9aaac29adcdba7201effc3665b1be9c0
Judson <nyarly@gmail.com>
2016-04-29 09:47:24 -0700

    Shorting the notify period

--- .config/fish/functions/notify_after_long.fish
+++ .config/fish/functions/notify_after_long.fish
@@ -6,7 +6,7 @@ function notify_after_long --on-event fish_postexec --argument last_command
   #set CMD_SECONDS (echo $CMD_DURATION | awk 'BEGIN { FS="." } ; { print $1 }')
   set -gx CMD_SECONDS (math $CMD_DURATION/1000)
   which notify-send > /dev/null
-  if test $status -eq 0 -a $CMD_SECONDS -gt 60
+  if test $status -eq 0 -a $CMD_SECONDS -gt 15
     notify-send "Long command complete" "The long running command\n  $last_command\n has finished.\nTotal duration: $CMD_SECONDS s"
     echo \a
   end
