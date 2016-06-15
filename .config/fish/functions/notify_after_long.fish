01a14bfa7430585c6dfd52a4e8127d6bf80dd419
Judson <nyarly@gmail.com>
2016-06-14 17:07:42 -0700

    Notify after long

--- .config/fish/functions/notify_after_long.fish
+++ .config/fish/functions/notify_after_long.fish
@@ -5,9 +5,8 @@ function notify_after_long --on-event fish_postexec --argument last_command
   end
   #set CMD_SECONDS (echo $CMD_DURATION | awk 'BEGIN { FS="." } ; { print $1 }')
   set -gx CMD_SECONDS (math $CMD_DURATION/1000)
-  which notify-send > /dev/null
-  if test $status -eq 0 -a $CMD_SECONDS -gt 15
-    notify-send "Long command complete" "The long running command\n  $last_command\n has finished.\nTotal duration: $CMD_SECONDS s"
+  if [ $CMD_SECONDS -gt 15 ]
+    notify "Long command complete" "The long running command\n  $last_command\n has finished.\nTotal duration: $CMD_SECONDS s"
     echo \a
   end
 end
