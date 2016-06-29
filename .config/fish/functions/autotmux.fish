0d1fbb408d9dfac1b93c8a15bcc03103bbc823e0
Judson <nyarly@gmail.com>
2016-06-29 16:49:56 -0700

    Notify after long

--- .config/fish/functions/autotmux.fish
+++ .config/fish/functions/autotmux.fish
@@ -1,7 +1,11 @@
 function autotmux --on-variable TMUX_SESSION_NAME
-  if test -n "$TMUX_SESSION_NAME" #only if set
+	if test -n "$TMUX_SESSION_NAME" #only if set
     if test -z $TMUX #not if in TMUX
-      tmux new-session -t "$TMUX_SESSION_NAME"; or tmux new-session -s "$TMUX_SESSION_NAME"
+      if tmux has-session -t "$TMUX_SESSION_NAME"
+        exec tmux new-session -t "$TMUX_SESSION_NAME"
+      else
+        exec tmux new-session -s "$TMUX_SESSION_NAME"
+      end
     end
   end
 end
