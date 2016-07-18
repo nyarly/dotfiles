41093f7a3059754f0104d1c438a82ee6090183c4
Judson <nyarly@gmail.com>
2016-07-18 16:51:59 -0700

    Adds dir history keybindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,10 @@
-
-if type -q fzf_key_bindings
-    fzf_key_bindings
-end
 function fish_user_key_bindings
-    bind \e\[1~ beginning-of-line
+	bind \eh prevd 'commandline -f repaint'
+  bind \el nextd 'commandline -f repaint'
+  bind \ek cd 'commandline -f repaint'
+  bind \ej 'cd -' 'commandline -f repaint'
+
+	bind \e\[1~ beginning-of-line
     bind \e\[3~ delete-char
     bind \e\[4~ end-of-line
     bind \e` clobber_history
