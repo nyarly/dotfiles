cf01c872c9335506b6691f6744baf6d19b6dd75c
Judson <nyarly@gmail.com>
2016-12-01 15:22:39 -0800

    Few cleanups

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
-    bind \eh prevd 'commandline -f repaint'
+	bind \eh prevd 'commandline -f repaint'
     bind \el nextd 'commandline -f repaint'
     bind \ek cd 'commandline -f repaint'
     bind \ej 'cd -' 'commandline -f repaint'
@@ -17,11 +17,13 @@ function fish_user_key_bindings
     bind \ct '__fzf_ctrl_t'
     bind \cr '__fzf_ctrl_r'
     bind \cx '__fzf_ctrl_x'
+    bind \ce '__fzf_ctrl_e'
     bind \ec '__fzf_alt_c'
     if bind -M insert > /dev/null ^ /dev/null
         bind -M insert \ct '__fzf_ctrl_t'
         bind -M insert \cr '__fzf_ctrl_r'
         bind -M insert \cx '__fzf_ctrl_x'
+        bind -M insert \ce '__fzf_ctrl_e'
         bind -M insert \ec '__fzf_alt_c'
     end
     ### fzf ###
