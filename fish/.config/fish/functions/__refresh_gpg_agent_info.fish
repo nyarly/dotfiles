4c344bfa8365e81d71a1dd45848911d91572f39f
Judson <nyarly@gmail.com>
2017-04-25 17:06:18 -0700

    Updating fish

--- fish/.config/fish/functions/__refresh_gpg_agent_info.fish
+++ fish/.config/fish/functions/__refresh_gpg_agent_info.fish
@@ -1,4 +1,7 @@
 function __refresh_gpg_agent_info --description 'Reload ~/.gpg-agent-info'
+	if test ! -c $GPG_TTY
+    set -x GPG_TTY (tty)
+  end
 	cat ~/.gpg-agent-info | sed 's/=/ /' | while read key value
         #echo $key $$key "->" $value
   set -e $key
