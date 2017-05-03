4ad029a7d088d95dad1c2f6943926c1574b7fa79
Judson <nyarly@gmail.com>
2017-05-03 09:35:52 -0700

    Adding fisherman completions to ignore, and a __fisher_ignore function

--- .config/fish/functions/__fisher_ignore.fish
+++ .config/fish/functions/__fisher_ignore.fish
@@ -1,4 +1,4 @@
 function __fisher_ignore
-	find ~/.config/fish/functions/ -type l | sed "s#"$HOME"##" >> ~/.gitignore.d/fish
-	find ~/.config/fish/completions/ -type l | sed "s#"$HOME"##" >> ~/.gitignore.d/fish
+	comm -13 (sort ~/.gitignore.d/fish | psub) (find ~/.config/fish/functions/ -type l | sed "s#"$HOME"##"|sort |psub) >> ~/.gitignore.d/fish
+	comm -13 (sort ~/.gitignore.d/fish | psub) (find ~/.config/fish/completions/ -type l | sed "s#"$HOME"##"|sort |psub) >> ~/.gitignore.d/fish
 end
