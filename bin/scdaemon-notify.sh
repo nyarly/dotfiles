466a9a10bb561b086d4dea872f15f2d6306b31f4
Judson <nyarly@gmail.com>
2017-09-28 09:04:04 -0700

    Nix-which didn't take an argument, scdaemon moving

--- bin/scdaemon-notify.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-nc -lU /tmp/scdaemon.sock | while read line; do
-  if echo $line | egrep -q 'PK(SIGN|AUTH)'; then
-    notify-send "GPG activity" "A process is waiting on the Yubikey!"
-  fi
-done
