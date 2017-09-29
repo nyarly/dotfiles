6c3f4a20f6dc45eff361a54f1745b2105517f9a5
Judson <nyarly@gmail.com>
2017-09-29 09:08:32 -0700

    Cleaning up flubbed gnupg

--- .gnupg/scdaemon-notify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source /etc/profile
-
-nc -lU /tmp/scdaemon.sock | while read line; do
-  if echo $line | egrep -q 'PK(SIGN|AUTH)'; then
-    notify-send "GPG activity" "A process is waiting on the Yubikey!"
-  fi
-done
