e296dedc78cbe4f67e9f9745eb9af063038e11c6
Judson <nyarly@gmail.com>
2017-10-09 08:59:17 -0700

    Updating gnupg

--- gnupg/.gnupg/scdaemon-notify.sh
+++ gnupg/.gnupg/scdaemon-notify.sh
@@ -5,5 +5,7 @@ source /etc/profile
 nc -lU /tmp/scdaemon.sock | while read line; do
   if echo $line | egrep -q 'PK(SIGN|AUTH)'; then
     notify-send "GPG activity" "A process is waiting on the Yubikey!"
+    echo "Notifying" >> /tmp/scdaemon.log
   fi
+  echo $line >> /tmp/scdaemon.log
 done
