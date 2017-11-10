2762ef08493d0826b76a2276f45377c03c2cdf32
Judson <nyarly@gmail.com>
2017-11-09 17:22:49 -0800

    Updating xmonad

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -10,6 +10,10 @@ import XMonad.Layout.Named
 import XMonad.Layout.NoBorders
 import XMonad.Layout.ToggleLayouts
 import XMonad.Actions.WindowBringer
+import qualified XMonad.Actions.CycleWS as C
+import qualified XMonad.StackSet as W
+
+-- c.f. http://hackage.haskell.org/package/xmonad-contrib-0.13/docs/XMonad-Actions-CycleWS.html
 
 import System.Taffybar.Hooks.PagerHints (pagerHints)
 
@@ -17,6 +21,8 @@ startup :: X ()
 startup = do
   spawn "systemctl --user restart xmonad.target"
   spawn "xss-lock -- i3lock -i ~/Data/Wallpaper/rotsnakes-tile.png -t"
+
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 myLayout = avoidStruts $ ifWider 1900 (toggle tall ||| full) (Mirror $ toggle tall ||| full)
   where
@@ -29,13 +35,17 @@ myLayout = avoidStruts $ ifWider 1900 (toggle tall ||| full) (Mirror $ toggle ta
 myGotoMenu = gotoMenuArgs ["-i", "-l", "45"]
 myBringMenu = bringMenuArgs ["-i", "-l", "45"]
 
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList [
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList ([
        ((modm, xK_z), spawn "i3lock -i ~/Data/Wallpaper/rotsnakes-tile.png -t &"),
        ((modm, xK_a), spawn "dmenu-screenlayout &"),
        ((modm, xK_grave), spawn "dmenu-scripts &"),
        ((modm, xK_g), myGotoMenu),
        ((modm, xK_b), myBringMenu)
-     ]
+     ] ++ [ ((modm, key), C.toggleOrView tag) |
+       (tag, key)  <- zip myWorkspaces [xK_1,xK_2,xK_3,xK_4,xK_5,xK_6,xK_7,xK_8,xK_9,xK_0]
+     ] ++ [ ((modm .|. shiftMask, key), (windows . W.shift) tag) |
+       (tag, key)  <- zip myWorkspaces [xK_1,xK_2,xK_3,xK_4,xK_5,xK_6,xK_7,xK_8,xK_9,xK_0]
+     ])
 newKeys x = myKeys x `M.union` keys def x
 
 myManageHook = composeAll [
@@ -53,5 +63,6 @@ main = xmonad $
            , layoutHook = myLayout
            , logHook = dynamicLogString defaultPP >>= xmonadPropLog
            , manageHook = myManageHook <+> manageHook def
+           , workspaces = myWorkspaces
            , keys = newKeys
            }
