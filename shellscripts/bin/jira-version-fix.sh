725a3234a4d0bc0e46da2a338e9991d6f5a9619a
Judson <nyarly@gmail.com>
2017-12-15 09:05:18 -0800

    Updating shellscripts

--- shellscripts/bin/jira-version-fix.sh
+++ shellscripts/bin/jira-version-fix.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+set -ex
 currentversion="nv_$(git tag | grep '[.].*[.*]' | versiontool sort | tail -n 1)"
 
-set -ex
 for id in $(jira-cli view --search-jql 'project = SOUS AND status = Closed and fixVersion is EMPTY' --format '%key'); do
   jira-cli update --fix-version "$currentversion" "$id"
 done
