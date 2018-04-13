1f4eadbaf086acdd71f214b235fd58b7411b60f7
Judson <nyarly@gmail.com>
2018-04-12 17:13:52 -0700

    Updating

--- fish/.config/fish/functions/cd_fasd_fzf.fish
+++ fish/.config/fish/functions/cd_fasd_fzf.fish
@@ -1,6 +1,10 @@
-# Defined in /tmp/fish.dOJ1rH/cd_fasd_fzf.fish @ line 2
+# Defined in /tmp/fish.XlnZEw/cd_fasd_fzf.fish @ line 2
 function cd_fasd_fzf
-	cd (fasd -ld | fzf)
+	set path (fasd -ld | fzf)
+  if test $status -ne 0
+    return
+  end
+  cd $path
   commandline -f repaint
   emit fish_prompt
 end
