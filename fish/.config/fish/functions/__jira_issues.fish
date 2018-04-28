57ac674927a364491080b473452dfab95537b590
Judson <nyarly@gmail.com>
2018-04-27 17:44:26 -0700

    Updating

--- fish/.config/fish/functions/__jira_issues.fish
+++ fish/.config/fish/functions/__jira_issues.fish
@@ -1,46 +1,13 @@
-# Defined in /run/user/1000/fish.YoVmmf/__jira_issues.fish @ line 2
+# Defined in /run/user/1000/fish.ViodIT/__jira_issues.fish @ line 2
 function __jira_issues
-	set -l username $JIRA_USERNAME
-  set -l password $JIRA_PASSWORD
-  set -l jql $JIRA_JQL
-  set -l jarfile $JIRA_JARFILE
-  set -l server $JIRA_SERVER
-  #'and assignee = currentUser()'
+	set -l jql $JIRA_JQL
 
-  if test -z $username
-    set username (git config --get jira.user)
+  if test -z $jql
+    set jql (git config --get jira.jql)
     if test $status -ne 0
-      return 1
+        return 1
     end
   end
 
-if test -z $password
-  set password (git config --get jira.password)
-  if test $status -ne 0
-    return 1
-end
-end
-
-if test -z $jarfile
-  set jarfile (git config --get jira.jarfile)
-  if test $status -ne 0
-    return 1
-end
- end
-
- if test -z $server
-   set server (git config --get jira.server)
-   if test $status -ne 0
-     return 1
-end
-end
-
-if test -z $jql
-  set jql (git config --get jira.jql)
-  if test $status -ne 0
-    return 1
-end
-end
-
-java -jar "$jarfile" --server "$server" --user "$username" --password "$password" --action getIssueList --columns key,summary,status --jql "$jql" | sed -E 's/^"|"$//g' | awk -F '"*,"*' 'NR > 2 && NF == 3 { print $1 "	" $2 " [" $3 "]" }'
+  jira-cli view --search-jql $jql --format '%key SEP %summary [%status]' ^/tmp/jira_issues.err | sed 's/ SEP /\t/'
 end
