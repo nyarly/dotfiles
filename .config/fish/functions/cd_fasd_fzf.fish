ea6703aad5282c4af85386f4510b7f819b5da22e
Judson <nyarly@gmail.com>
2018-05-10 10:16:32 -0700

    cd_fash_fzf order

--- .config/fish/functions/cd_fasd_fzf.fish
+++ .config/fish/functions/cd_fasd_fzf.fish
@@ -1,6 +1,6 @@
-# Defined in /tmp/fish.XlnZEw/cd_fasd_fzf.fish @ line 2
+# Defined in /tmp/fish.PDijWr/cd_fasd_fzf.fish @ line 2
 function cd_fasd_fzf
-	set path (fasd -ld | fzf)
+	set path (fasd -ld | fzf --no-sort --tac)
   if test $status -ne 0
     return
   end
