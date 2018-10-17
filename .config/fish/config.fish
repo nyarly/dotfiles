a76e5be56d37d524dda3a14973d927c68721c8a3
Judson <nyarly@gmail.com>
2018-10-17 11:24:35 -0700

    Conditioning some config on interactive shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,12 @@
 ulimit -n 4096
 
 source ~/.config/fish/functions/capture_status.fish
-source ~/.config/fish/functions/notify_after_long.fish
-
-set -eg EDITOR # Use set -xU EDITOR and VISUAL
 
 set -g __fish_git_prompt_show_informative_status yes
 
+set -eg EDITOR # Use set -xU EDITOR and VISUAL
 set -gx PAGER "less -RF"
-
-# Preferring xdg-open ...
-#set -x BROWSER chromium-browser
-#set -x CHROME_BIN /usr/bin/chromium
-
 set -gx MANPATH "" $MANPATH /run/current-system/sw/share/man
-
 set -gx RIPGREP_CONFIG_PATH ~/.config/ripgreprc
 
 if test -f ~/.config/fish/private.fish
@@ -22,23 +14,24 @@ if test -f ~/.config/fish/private.fish
 end
 
 # The shell is my partner - it needs tools too:
-eval (direnv hook fish)
 if [ -f /usr/local/share/chruby/chruby.fish ]
   source /usr/local/share/chruby/chruby.fish
 end
 
-source ~/.config/fish/functions/autotmux.fish
-source ~/.config/fish/functions/_run_fasd.fish
 source ~/.config/fish/nix.fish
 source ~/.config/fish/go.fish
 source ~/.config/fish/rust.fish
 
-dynamic-colors init
-
-# Alt-; converts filname:1234 -> filename +1234 for easy backtrace jumping
-bind \e\; 'commandline -r -t (commandline -t | sed "s/:\(\d*\)/ +\1/")'
-
-set -x fish_color_search_match  'normal' '--background=878787'
+if status is-interactive
+  source ~/.config/fish/functions/notify_after_long.fish
+  eval (direnv hook fish)
+  source ~/.config/fish/functions/autotmux.fish
+  source ~/.config/fish/functions/_run_fasd.fish
+  dynamic-colors init
+  # Alt-; converts filname:1234 -> filename +1234 for easy backtrace jumping
+  bind \e\; 'commandline -r -t (commandline -t | sed "s/:\(\d*\)/ +\1/")'
+  set -x fish_color_search_match  'normal' '--background=878787'
+end
 
 function fish_greeting
 end
