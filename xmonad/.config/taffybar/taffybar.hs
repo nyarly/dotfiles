a058ca3b8c2dd50cb4788ee7ee3e81d72d2fac42
Judson <nyarly@gmail.com>
2019-01-09 23:53:30 -0800

    Updating

--- xmonad/.config/taffybar/taffybar.hs
+++ xmonad/.config/taffybar/taffybar.hs
@@ -1,22 +1,15 @@
 import System.Taffybar
-
-import System.Taffybar.Widget.SNITray
-import System.Taffybar.Widget.Workspaces
-import System.Taffybar.Widget.Layout
-import System.Taffybar.Widget.Windows
-import System.Taffybar.Widget.SimpleClock
-import System.Taffybar.Widget.FreedesktopNotifications
-import System.Taffybar.Widget.Weather
-import System.Taffybar.Widget.MPRIS2
-import System.Taffybar.Widget.Battery
-
--- import System.Taffybar.Widgets.PollingBar
--- import System.Taffybar.Widgets.PollingGraph
-
-import System.Taffybar.Information.Memory
+import System.Taffybar.Hooks
 import System.Taffybar.Information.CPU
-import System.Taffybar.Information.Battery
+import System.Taffybar.Information.Memory
+import System.Taffybar.SimpleConfig
+import System.Taffybar.Widget
+import System.Taffybar.Widget.Generic.PollingGraph
+import System.Taffybar.Widget.Generic.PollingLabel
+import System.Taffybar.Widget.Util
+import System.Taffybar.Widget.Workspaces
 
+import Data.Text(pack)
 import Data.Time.LocalTime(utc)
 -- import Data.Text
 
@@ -25,47 +18,51 @@ memCallback = do
   return [memoryUsedRatio mi]
 
 cpuCallback = do
-  (userLoad, systemLoad, totalLoad) <- cpuLoad
-  return [totalLoad, systemLoad]
+  (_, systemLoad, totalLoad) <- cpuLoad
+  return [ totalLoad, systemLoad ]
 
 main = do
-  let memCfg = defaultGraphConfig {
-          graphLabel = Nothing -- Just "mem"
+  let
+    graphCfg = defaultGraphConfig {
+      graphLabel = Just (pack "mem")
         , graphDirection = RIGHT_TO_LEFT
         , graphWidth = 40
+    }
+    memCfg = graphCfg {
+      graphLabel = Just (pack "mem")
         , graphDataColors = [(1, 0, 0, 1)]
-      }
-      cpuCfg = defaultGraphConfig {
-          graphLabel = Nothing -- Just "cpu"
-        , graphDirection = RIGHT_TO_LEFT
-        , graphWidth = 40
-        , graphDataColors = [
-            (0, 1, 0, 1)
-          , (1, 0, 1, 0.5)
-          ]
-      }
-  let clock = textClockNew Nothing "<span fgcolor='orange'>%a %b %_d %H:%M</span>" 1
-      zebra = textClockNewWith (ClockConfig (Just utc) Nothing) "<span fgcolor='silver'>%a %b %_d %H:%M</span>" 1
-      pager = taffyPagerHUDNew defaultPagerConfig {
-        activeLayout = escape . take 1
-      , activeWindow = escape . shorten 20
-      } defaultWorkspaceHUDConfig { windowIconSize = 12
-      , minWSWidgetSize = Just 25
-      , maxIcons = Just 1
-      , showWorkspaceFn = hideEmpty
-      , urgentWorkspaceState = True
-      }
-      -- note = notifyAreaNew defaultNotificationConfig
-      -- wea = weatherNew (defaultWeatherConfig "KMSN") 10
-      mpris = mprisNew defaultMPRISConfig
-      mem = pollingGraphNew memCfg 1 memCallback
-      cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
-      tray = systrayNew
-      -- bat = textBatteryNew "%d%%" 1
-      batt = batteryBarNew defaultBatteryConfig 30
-  defaultTaffybar defaultTaffybarConfig { barPosition = Bottom
-                                        , getMonitorConfig = allMonitors
-                                        , startWidgets = [ pager ]
-                                        , barHeight = 28
-                                        , endWidgets = [ tray, zebra, clock,  batt, mem, cpu, mpris ]
-                                        }
+    }
+    cpuCfg = graphCfg {
+      graphLabel = Just (pack "cpu")
+        , graphDataColors = [ (0, 1, 0, 1), (1, 0, 1, 0.5)]
+    }
+
+    myWorkspacesConfig = defaultWorkspacesConfig {
+      minIcons = 1
+        , widgetGap=0
+        , showWorkspaceFn = hideEmpty
+    }
+    workspaces = workspacesNew myWorkspacesConfig
+    clock = textClockNew Nothing "<span fgcolor='orange'>%a %b %_d P %H:%M</span>" 1
+    zebra = textClockNewWith (ClockConfig (Just utc) Nothing) "<span fgcolor='silver'>%a %b %_d Z %H:%M</span>" 1
+    -- The right way seems to set up an sni listener in systemd?
+    tray = sniTrayThatStartsWatcherEvenThoughThisIsABadWayToDoIt
+    -- tray = sniTrayNew
+    cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+    mem = pollingGraphNew memCfg 1 memCallback
+    -- batt = batteryBarNew defaultBatteryConfig 30
+    batt = textBatteryNew "$percentage$% ($time$)"
+    layout = layoutNew defaultLayoutConfig
+    {-
+     - the menu widget doesn't seem to accept a height?
+     - or better still, respect the height of the bar it's put in.
+    windows = windowsNew defaultWindowsConfig
+    -}
+
+    simpleConfig = defaultSimpleTaffyConfig {
+      startWidgets = [ workspaces, layout ] --, layout, windows ]
+        , endWidgets = [ tray, zebra, clock, mem, cpu, batt ]
+        , barHeight = 20
+        , barPosition = Bottom
+    }
+  dyreTaffybar $ toTaffyConfig simpleConfig
