4d6215c47f8496a51d9aa73f9f2a7b6b907f316c
Judson <nyarly@gmail.com>
2019-01-23 08:44:07 -0800

    Updating

--- fish/.config/fish/functions/cd_fasd_fzf.fish
+++ fish/.config/fish/functions/cd_fasd_fzf.fish
@@ -4,6 +4,18 @@ function cd_fasd_fzf
   if test $status -ne 0
     return
   end
+  if wmctrl -l | grep -q $path
+    set window (begin
+      wmctrl -l | grep $path
+      echo "Don't"
+    end | fzf --prompt "Use existing window?> ")
+    if test $status -eq 0; and test $window != "Don't"
+      set wname (echo $window | awk '{ print $1 }')
+      wmctrl -i -a $wname
+      return
+    end
+  end
+
   cd $path
   commandline -f repaint
   emit fish_prompt
